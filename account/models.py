138829d7b7baf4276e23e1ecea60363049b22bfb
PeymanNr <peyman.naseri7@gmail.com>
2023-05-05 13:05:40 +0330

    modified models

--- account/models.py
+++ account/models.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
 class Author(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE, related_name='authors')
+
+    def __str__(self):
+        return self.author
+
+    class Meta:
+        verbose_name = _('author')
+        verbose_name_plural = _("authors")
 
 
 
