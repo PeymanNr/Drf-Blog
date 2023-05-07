dcef0d3a699fc28ff8c11fe1dbc385d7ff8e9049
PeymanNr <peyman.naseri7@gmail.com>
2023-05-07 14:28:39 +0330

    add pagination class

--- Blog/settings.py
+++ Blog/settings.py
@@ -149,6 +149,6 @@ REST_FRAMEWORK = {
     ) if DEBUG else (
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 1
 }
